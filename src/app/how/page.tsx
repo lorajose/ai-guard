2f23eb18726fefcefd120a66039ce2bb5d90c7bb
joselora <jsloravargas@gmail.com>
2025-12-06 11:43:22 -0500

    Removed the unsupported dynamic import from the How page so the build succeeds again. Page now just imports the client component normally:

--- src/app/how/page.tsx
+++ src/app/how/page.tsx
@@ -1,9 +1,4 @@
-import dynamic from "next/dynamic";
-
-const HowItWorksLanding = dynamic(() => import("@/components/HowItWorksLanding"), {
-  ssr: false,
-  loading: () => <div className="min-h-screen bg-black" />,
-});
+import HowItWorksLanding from "@/components/HowItWorksLanding";
 
 export default function HowPage() {
   return <HowItWorksLanding />;
